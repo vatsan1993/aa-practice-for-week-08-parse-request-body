07a2caa807d961bd38d4bcc512fde5a8c6b1080c
Srivatsan <vatsan1993@gmail.com>
2024-04-12 16:06:25 +0530

    finished project

--- parse-body.js
+++ parse-body.js
@@ -1,25 +1,54 @@
 function firstStep(input) {
   // Your code here
+
+  let parts = input.split('&');
+  return parts;
 }
 
 function secondStep(input) {
   // Your code here
+  let dividedParts = input.map((part) => {
+    return part.split('=');
+  });
+  return dividedParts;
 }
 
 function thirdStep(input) {
   // Your code here
+  let fixedInput = input.map((part) => {
+    return [part[0], part[1].replace(/\+/g, ' ')];
+  });
+  return fixedInput;
 }
 
 function fourthStep(input) {
   // Your code here
+  let fixedInput = input.map((part) => {
+    return [part[0], decodeURIComponent(part[1])];
+  });
+  return fixedInput;
 }
 
 function fifthStep(input) {
   // Your code here
+  let body = input.reduce((acc, part) => {
+    let key = part[0];
+    let value = part[1];
+    let obj = {};
+    obj[key] = value;
+    let newObj = Object.assign(acc, obj);
+    return newObj;
+  }, {});
+  return body;
 }
 
 function parseBody(str) {
   // Your code here
+  let parts = firstStep(str);
+  let subParts = secondStep(parts);
+  let fixedParts = thirdStep(subParts);
+  let decodedParts = fourthStep(fixedParts);
+  return fifthStep(decodedParts);
 }
 
 /******************************************************************************/
@@ -31,5 +60,5 @@ module.exports = {
   thirdStep,
   fourthStep,
   fifthStep,
-  parseBody
+  parseBody,
 };
--- server.js
+++ server.js
@@ -1,12 +1,33 @@
-const { sendFormPage } = require("./routes");
-const { parseBody } = require("./parse-body");
+const { sendFormPage } = require('./routes');
+const { parseBody } = require('./parse-body');
 let server;
 
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE ABOVE THIS LINE *******************/
 
 // Your code here
+const http = require('http');
+const PORT = 5000;
+server = http.createServer((req, res) => {
+  console.log(`${req.method} ${req.url}`);
+  let reqBody = '';
 
+  req.on('data', (data) => {
+    reqBody += data;
+  });
+  req.on('end', () => {
+    if (reqBody != '') {
+      let obj = parseBody(reqBody);
+      console.log(obj);
+      req.body = obj;
+    }
+
+    sendFormPage(req, res);
+  });
+});
+server.listen(PORT, () => {
+  console.log(`Successfully started the server on port ${PORT}`);
+});
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE BELOW THIS LINE *******************/
 
